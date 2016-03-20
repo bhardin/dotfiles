666706095171aadaec49ad6f9c97ee7c763f4564
Brett Hardin <bhardin@kissmetrics.com>
2016-03-20 07:32:01 -0700

    Bugfix: Ordering path correctly

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSHDOT/bin:$PATH"
+export PATH="$ZSHDOT/bin:$PATH:./bin:/usr/local/bin:/usr/local/sbin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
