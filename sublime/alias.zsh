bd29308cb2abbf5485a61372482181b40b0bee1e
Brett Hardin <brett.hardin@esharesinc.com>
2016-04-19 10:56:17 -0700

    Improvement: Creating sublime shortcut

---
+++ sublime/alias.zsh
@@ -0,0 +1,8 @@
+if [[ $('uname') == 'Linux' ]]; then
+  runst() { nohup /usr/bin/sublime-text-2 $@ > /dev/null & }
+  alias st=runst
+elif  [[ $('uname') == 'Darwin' ]]; then
+  alias st='open -a /Applications/Sublime\ Text\ 2.app'
+fi
+        
+alias stt='st .'
