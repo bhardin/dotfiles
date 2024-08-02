8735745228551143870ea8fe1cd7f7220507bc13
Brett Hardin <brett.hardin@joincontinuum.com>
2024-08-02 10:48:33 -0700

    Temp comments

--- python/path.zsh
+++ python/path.zsh
@@ -1,6 +1,9 @@
 # If using python virtual environments
 eval "$(pyenv init -)"
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+
+# if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
 # Poetry
-export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
