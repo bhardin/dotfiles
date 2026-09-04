758651cda4175a9b866f072700d5aa2a3c8061d7
Brett Hardin <brett@getlemma.com>
2026-09-04 16:01:28 -0700

    Stuff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -25,10 +25,6 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 # Replicates "git compare" from hub
 git config --global alias.compare "!gh pr create --web"
 
-# Common ones I use
-alias git-oneline='git log --oneline --reverse'
-
-
 ## Delete merged branches, prune, and remove empty directories
 # alias git-clean='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d && git remote prune origin && find . -type d -empty -delete'
 
